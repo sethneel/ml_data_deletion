80c26714fdadc255c5b8a476859e38f18332072b
svnscholar <sethneel@wharton.upenn.edu>
2020-08-30 21:26:13 -0400

    wip finished reservoir sampling

--- fed_desc_del.py
+++ fed_desc_del.py
@@ -17,6 +17,7 @@ class FedDescDel:
         # dictionary where entry k is the indices in the kth partition
         self.K = K
         self.B = B
+        self.bootstrap_size = int(float(B)/float(K))
         n = X_train.shape[0]
         m = int(np.round(float(B)/float(K)))
         self.bootstrap = {f'{x}': np.random.choice(size=m, a=n, replace=True) for x in range(K)}
@@ -24,9 +25,10 @@ class FedDescDel:
         self.y_train = y_train
         self.y_u = y_train
         self.y_test = y_test
+        # list of dictionaries
         self.models = []
+        # list of dictionriers
         self.noisy_models = []
-        self.scratch_models = []
         self.epsilon = epsilon
         self.delta = delta
         self.sigma = 0
@@ -42,13 +44,15 @@ class FedDescDel:
 
     def update(self, update):
         """Given update, output retrained model, noisy and secret state"""
+
+        # update X_u
         self.update_data_set(update)
-        new_model = self.train(iters=self.update_grad_iter, init=self.models[-1])
-        new_model_scratch = self.train(iters=self.update_grad_iter, init=None)
-        noisy_model = self.publish(new_model)
-        self.models.append(new_model)
-        self.noisy_models.append(noisy_model)
-        self.scratch_models.append(new_model_scratch)
+        # update boostrap sample
+        updated_partitions = self.reservoir_sampling_update(update)
+        # retrain on updated partitions - update models
+        self.train_partitions(self.update_grad_iter, updated_partitions, init_dict=self.models[-1])
+        # calculate noisy theta (STOPPED HERE NEED TO IMPLEMENT)
+        self.publish(self.models[-1])
         self.model_accuracies.append(self.get_test_accuracy(noisy_model))
         self.scratch_model_accuracies.append(self.get_test_accuracy(new_model_scratch))
 
@@ -65,7 +69,7 @@ class FedDescDel:
                                                                      np.sqrt(np.log(1/self.delta)))
         self.sigma = sigma_numerator/sigma_denominator
 
-    def train(self, iters, init=None):
+    def train(self, X_u, y_u, iters, init=None):
         """In initial round """
         if init:
             model = self.model_class(init.theta, l2_penalty=self.l2_penalty)
@@ -74,13 +78,44 @@ class FedDescDel:
             par = par/(np.sqrt(np.sum(np.power(par, 2))))
             model = self.model_class(par, l2_penalty=self.l2_penalty)
         for _ in range(iters):
-            model.proj_gradient_step(self.X_u, self.y_u)
+            model.proj_gradient_step(X_u, y_u)
         return model
+
+    def train_partitions(self, iters, updated_partitions, init_dict):
+        new_model_dict = self.models[-1]
+        for part in updated_partitions:
+            X_part = self.X_u.iloc[self.bootstrap[part], :]
+            y_part = self.y_u.iloc[self.bootstrap[part]]
+            new_model_dict[part] = self.train(X_part, y_part, iters, init_dict[part])
+        self.models.append(new_model_dict)
 
     def publish(self, model):
         noise = np.random.normal(0, self.sigma, self.datadim)
         theta = model.theta + noise
         return self.model_class(theta, l2_penalty=self.l2_penalty)
+
+    def reservoir_sampling_update(self, update):
+        """Update the bootstrap sample. Assumes self.X_u already updated."""
+        # track and output partitions updated during this step
+        updated_partitions = []
+        if update[0] == '-':
+            for key, value in self.bootstrap:
+                if update[1] in value:
+                    updated_partitions.append(key)
+                    self.bootstrap[key] = remove(update[1], value, self.X_u.index)
+        else:
+            # number of indices to be replaced
+            N = np.random.binomial(self.B, 1.0/self.X_u.shape[0])
+            replacement_indices = np.random.choice(self.B, N, replace=False)
+            for key, value in self.bootstrap:
+                index_interval = range(np.int(key)*len(self.bootstrap_size), (np.int(key) + 1)*len(self.bootstrap_size))
+                adjusted_ind = [r - np.int(key)*len(self.bootstrap_size) for r in replacement_indices
+                                if r in index_interval]
+                if len(adjusted_ind) > 0:
+                    updated_partitions.append(key)
+                    self.bootstrap[key][adjusted_ind] = update[1]
+        return updated_partitions
+
 
     def update_data_set(self, update):
         # Implement Reservoir Sampling:
@@ -116,16 +151,6 @@ class FedDescDel:
         y_hat = model.predict(self.X_test)
         return np.float(np.sum([np.array(y_hat) == np.array(self.y_test)]))/np.float(len(self.y_test))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+def remove(index_to_remove, index_array, sample_indices):
+    """remove index_to_remove from index_array and replace by a sample from sample indices """
+    return [ind if ind != index_to_remove else np.random.choice(sample_indices) for ind in index_array]
